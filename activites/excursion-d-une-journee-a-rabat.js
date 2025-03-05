05b0175aa80775701fcfe1a7b746a28e5317d231
NajatMezough <162812013+NajatMezough@users.noreply.github.com>
2025-03-05 16:31:13 +0100

    TLSS-16: add updat all file

--- activites/excursion-d-une-journee-a-rabat.js
+++ activites/excursion-d-une-journee-a-rabat.js
@@ -106,3 +106,20 @@ document.addEventListener("DOMContentLoaded", function() {
         return prices[serviceType] * groupSize;
     }
 });
+
+const plus = document.querySelector(".plus"),
+                minus = document.querySelector(".minus"),
+                num = document.querySelector(".num");
+                let a = 1;
+                plus.addEventListener("click", ()=>{
+                  a++;
+                  a = (a < 10) ? "0" + a : a;
+                  num.innerText = a;
+                });
+                minus.addEventListener("click", ()=>{
+                  if(a > 1){
+                    a--;
+                    a = (a < 10) ? "0" + a : a;
+                    num.innerText = a;
+                  }
+                });
